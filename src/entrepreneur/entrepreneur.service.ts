b0054b1cd333f8eb4c974f90c81374ce1c7f0973
EmmanuelStocco <emmanuelrolimstocco@gmail.com>
2025-04-23 13:13:48 -0300

    feat: enterpreneur CRUD service

---
+++ src/entrepreneur/entrepreneur.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateEntrepreneurDto } from './dto/create-entrepreneur.dto';
+import { UpdateEntrepreneurDto } from './dto/update-entrepreneur.dto';
+import { slugify } from 'src/utils'; 
+
+@Injectable()
+export class EntrepreneurService {
+    constructor(private prisma: PrismaService) { }
+
+    create(data: CreateEntrepreneurDto) {
+        const {
+            userId,
+            businessName,
+            themeColor,
+            logoUrl,
+            description,
+            availableDays,
+            availableHours,
+            services,
+        } = data;
+
+        const slug = slugify(businessName);
+
+        return this.prisma.entrepreneurProfile.create({
+            data: {
+                userId,
+                businessName,
+                themeColor,
+                logoUrl,
+                description,
+                availableDays,
+                availableHours,
+                services,
+                slug,
+            },
+        });
+    }
+
+
+    findAll() {
+        return this.prisma.entrepreneurProfile.findMany({
+            include: { user: true },
+        });
+    }
+
+    findOne(id: number) {
+        return this.prisma.entrepreneurProfile.findUnique({
+            where: { id },
+            include: { user: true },
+        });
+    }
+
+    update(id: number, data: UpdateEntrepreneurDto) {
+        return this.prisma.entrepreneurProfile.update({
+            where: { id },
+            data,
+        });
+    }
+
+    remove(id: number) {
+        return this.prisma.entrepreneurProfile.delete({
+            where: { id },
+        });
+    }
+}
