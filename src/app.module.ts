22a8e145531a2a1a55648a5514dc43efbae5b66b
EmmanuelStocco <emmanuelrolimstocco@gmail.com>
2025-04-28 16:44:29 -0300

    feat: add appointment on app

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,11 @@ import { UserModule } from './user/user.module';
 import { PrismaService } from './prisma/prisma.service';
 import { AuthModule } from './auth/auth.module';
 import { EntrepreneurModule } from './entrepreneur/entrepreneur.module';
+import { AppointmentModule } from './appointment/appointment.module';
+
 
 @Module({
-  imports: [UserModule, AuthModule, EntrepreneurModule],
+  imports: [UserModule, AuthModule, EntrepreneurModule, AppointmentModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
