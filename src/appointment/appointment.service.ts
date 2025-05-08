ecef70c40c152e8da21ca6e6cec8435fd5cd0b3a
EmmanuelStocco <emmanuelrolimstocco@gmail.com>
2025-05-08 16:28:58 -0300

    feat: validation of existence of already scheduled date

--- src/appointment/appointment.service.ts
+++ src/appointment/appointment.service.ts
@@ -2,24 +2,75 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateAppointmentDto } from './dto/create-appointment.dto';
 import { UpdateAppointmentDto } from './dto/update-appointment.dto';
-
+import { BadRequestException } from '@nestjs/common';
+import { Errors } from 'src/common/errors';
 @Injectable()
 export class AppointmentService {
   constructor(private prisma: PrismaService) {}
 
-  create(data: CreateAppointmentDto) {
-    const { date, ...rest } = data;
+  async create(data: CreateAppointmentDto) {
+    const { date, time, entrepreneurId } = data;
   
     const appointmentDate = new Date(date);
-   
+  
+    const existing = await this.prisma.appointment.findFirst({
+      where: {
+        date: appointmentDate,
+        time,
+        entrepreneurId
+      },
+    });
+  
+    if (existing) {
+      throw Errors.AppointmentConflict()
+    }
+  
     return this.prisma.appointment.create({
       data: {
+        ...data,
+        date: appointmentDate,
+      },
+    });
+  } 
+
+  async update(id: number, data: UpdateAppointmentDto) {
+    const { date, time, ...rest } = data;
+  
+    if (!date || !time) {
+      return this.prisma.appointment.update({
+        where: { id },
+        data: rest,
+      });
+    }
+  
+    const appointmentDate = new Date(date); 
+    const existing = await this.prisma.appointment.findFirst({
+      where: {
+        id: { not: id }, 
+        date: appointmentDate,
+        time,
+      },
+    });
+  
+    if (existing) {
+      throw Errors.AppointmentConflict();
+    }
+  
+    return this.prisma.appointment.update({
+      where: { id },
+      data: {
         ...rest,
-        date: appointmentDate
+        date: appointmentDate,
+        time,
       },
     });
   }
-  
+   
+  remove(id: number) {
+    return this.prisma.appointment.delete({
+      where: { id },
+    });
+  } 
 
   findAll() {
     return this.prisma.appointment.findMany({
@@ -31,33 +82,6 @@ export class AppointmentService {
     return this.prisma.appointment.findUnique({
       where: { id },
       include: { client: true, entrepreneur: true },
-    });
-  }
-
-  update(id: number, data: UpdateAppointmentDto) { 
-    const { date, time, ...rest } = data;
-    if(!date || !time) {
-      return this.prisma.appointment.update({
-        where: { id },
-        data: {
-          ...rest,
-        }
-      });
-    }
-
-    const appointmentDate = new Date(date);
-    return this.prisma.appointment.update({
-      where: { id },
-      data: {
-        ...rest,
-        date: appointmentDate
-      }
-    });  
-  }
-
-  remove(id: number) {
-    return this.prisma.appointment.delete({
-      where: { id },
     });
   }
 }
